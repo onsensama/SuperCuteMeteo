57fec2b615a0e6315300219ffbb7826a4f7d7159
Tommy Laurent <tommylaurent@pm.me>
2021-08-06 23:45:05 +0200

    dynamic main image

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
-# V-02
+# SuperCute Meteo
+
+App VanillaJS V-02
+
+Application giving the current weather, the weather of the coming hours and the weather of the coming days based on the user's location.
+
+## Install
 
 - Install modules with npm i in your terminal
 - Download live sass compiler extension on vscode for read scss
 - Download live serveur extension on vscode for start app in local
+
+## Credits
+
+[Winter vector created by pch.vector](https://www.freepik.com/vectors/winter)
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const APIkey = "3cc044c4c340b1f70cd8b53a70460865";
-let resultsAPI;
+let dataMeteo;
 
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
@@ -9,7 +9,7 @@ if (navigator.geolocation) {
         lon: position.coords.longitude,
       };
 
-      getAPI(location.lat, location.lon);
+      CallAPI(location.lat, location.lon);
     },
     () => {
       alert(
@@ -19,9 +19,58 @@ if (navigator.geolocation) {
   );
 }
 
-function getAPI(lat, lon) {
+function CallAPI(lat, lon) {
   fetch(`
-https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely&units=metric&lang=fr&appid=${APIkey}`)
+https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely&units=metric&appid=${APIkey}`)
     .then((res) => res.json())
-    .then((data) => console.log(data));
+    .then((data) => {
+      dataMeteo = data;
+
+      console.log(dataMeteo);
+
+      dataDisplay(dataMeteo);
+    });
+}
+
+function dataDisplay(dataMeteo) {
+  displayMainPicture(dataMeteo);
+}
+
+function displayMainPicture(dataMeteo) {
+  const weatherImg = document.querySelector(".weather-main-picture");
+  const currentweather = dataMeteo.current.weather[0].description;
+
+  switch (currentweather) {
+    case "clear sky":
+      weatherImg.src = "/assets/img/weather/clear_sky.png";
+      break;
+    case "broken clouds":
+      weatherImg.src = "/assets/img/weather/broken_clouds.png";
+      break;
+    case "mist":
+      weatherImg.src = "/assets/img/weather/mist.png";
+      break;
+    case "rain":
+      weatherImg.src = "/assets/img/weather/rain.png";
+      break;
+    case "scattered clouds":
+      weatherImg.src = "/assets/img/weather/scattered_clouds.png";
+      break;
+    case "shower rain":
+      weatherImg.src = "/assets/img/weather/shower_rain.png";
+      break;
+    case "snow":
+      weatherImg.src = "/assets/img/weather/snow.png";
+      break;
+    case "thunderstorm":
+      weatherImg.src = "/assets/img/weather/thunderstorm.png";
+      break;
+    case "wind":
+      weatherImg.src = "/assets/img/weather/wind.png";
+      break;
+
+    default:
+      console.log("problemos");
+      break;
+  }
 }
