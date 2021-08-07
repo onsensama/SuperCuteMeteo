c5900a97413f64d80cb33b29f0d6c6650b36e59b
Tommy Laurent <tommylaurent@pm.me>
2021-08-07 17:58:35 +0200

    display current weather infos

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const APIkey = "3cc044c4c340b1f70cd8b53a70460865";
+
 let dataMeteo;
 
 if (navigator.geolocation) {
@@ -28,58 +29,25 @@ https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=mi
 
       console.log(dataMeteo);
 
-      dataDisplay(dataMeteo);
+      displayInfosWeather(dataMeteo);
     });
 }
 
-function dataDisplay(dataMeteo) {
-  displayMainPicture(dataMeteo);
+function displayInfosWeather(dataMeteo) {
+  currentInfos(dataMeteo);
+  // hoursInfos();
+  // daysInfos();
 }
 
-function displayMainPicture(dataMeteo) {
-  const weatherImg = document.querySelector(".weather-main-picture");
+function currentInfos(dataMeteo) {
+  const weatherImg = document.querySelector(".current-weather-picture");
+  const degree = document.querySelector(".current-degree");
+  const weatherText = document.querySelector(".current-weather-text");
   const currentweather = dataMeteo.current.weather[0].icon;
+  const currentDegree = dataMeteo.current.temp;
+  const currentWeather = dataMeteo.current.weather[0].description;
 
-  switch (currentweather) {
-    case "01d":
-    case "01n":
-      weatherImg.src = "/assets/img/weather/01d.png";
-      break;
-    case "02d":
-    case "02n":
-      weatherImg.src = "/assets/img/weather/02d.png";
-      break;
-    case "03d":
-    case "03n":
-      weatherImg.src = "/assets/img/weather/03d.png";
-      break;
-    case "04d":
-    case "04n":
-      weatherImg.src = "/assets/img/weather/04d.png";
-      break;
-    case "09d":
-    case "09n":
-      weatherImg.src = "/assets/img/weather/09d.png";
-      break;
-    case "10d":
-    case "10n":
-      weatherImg.src = "/assets/img/weather/10d.png";
-      break;
-    case "11d":
-    case "11n":
-      weatherImg.src = "/assets/img/weather/11d.png";
-      break;
-    case "13d":
-    case "13n":
-      weatherImg.src = "/assets/img/weather/13d.png";
-      break;
-    case "50d":
-    case "50n":
-      weatherImg.src = "/assets/img/weather/50d.png";
-      break;
-
-    default:
-      console.log("problemos");
-      break;
-  }
+  weatherImg.src = `assets/img/weather/${currentweather}.png`;
+  degree.innerText = `${Math.round(currentDegree)}º`;
+  weatherText.innerText = `${currentWeather}`;
 }
