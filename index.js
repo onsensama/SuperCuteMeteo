f788ea71880773154f7b8c0746407b68a6569134
Tommy Laurent <tommylaurent@pm.me>
2021-08-08 11:11:56 +0200

    display dynamic hours forecast

--- index.js
+++ index.js
@@ -29,17 +29,17 @@ https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=mi
 
       console.log(dataMeteo);
 
-      displayInfosWeather(dataMeteo);
+      displayWeather(dataMeteo);
     });
 }
 
-function displayInfosWeather(dataMeteo) {
-  currentInfos(dataMeteo);
-  // hoursInfos();
-  // daysInfos();
+function displayWeather(dataMeteo) {
+  currentWeather(dataMeteo);
+  forecastHours(dataMeteo);
+  // forecastDays();
 }
 
-function currentInfos(dataMeteo) {
+function currentWeather(dataMeteo) {
   const weatherImg = document.querySelector(".current-weather-picture");
   const degree = document.querySelector(".current-degree");
   const weatherText = document.querySelector(".current-weather-text");
@@ -50,4 +50,26 @@ function currentInfos(dataMeteo) {
   weatherImg.src = `assets/img/weather/${currentweather}.png`;
   degree.innerText = `${Math.round(currentDegree)}º`;
   weatherText.innerText = `${currentWeather}`;
+}
+
+function forecastHours(dataMeteo) {
+  const hours = document.querySelectorAll(".hour");
+  const degrees = document.querySelectorAll(".degree");
+  let currentHour = new Date().getHours();
+
+  for (let i = 0; i < hours.length; i++) {
+    let hour = currentHour + i * 3;
+    if (hour === 24) {
+      hours[i].innerText = `00h`;
+    } else if (hour > 24) {
+      hours[i].innerText = `${hour - 24}h`;
+    } else {
+      hours[i].innerText = `${hour}h`;
+    }
+  }
+
+  for (let i = 0; i < degrees.length; i++) {
+    const degree = dataMeteo.hourly[i * 3].temp;
+    degrees[i].innerText = `${Math.round(degree)}º`;
+  }
 }
